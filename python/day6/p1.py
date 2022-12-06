f69027ed08ee1d6abda7f2903f1621ad7e26fd50
Goldeck <younesnew2@gmail.com>
2022-12-06 11:53:31 +0100

    refactor and solve problem 2

--- python/day6/p1.py
+++ python/day6/p1.py
@@ -1,32 +1,20 @@
 f = open("inputOne.txt")
 content = f.read().strip()
 
-# z = "wxyz"
-# y = "zxww"
-# thisset = {"apple", "banana", "cherry"}
-# thisset.add("apple")
-# print(thisset)
-# print( z[0] in  {z[1] , z[2] , z[3]})
-# print( y[0] in  {y[1] , y[2] , y[3]})
-
-
 def firstMarker(message):
-	#create a set and add elements to it till it lenght is 3 insead of 4
-	marker = set()
-	markerLenght = 4
-	result = 0
-	
+	marker = set()	
+	markerLenght = 14
 	cpt = 0
 	for lettre in message:
+		temp = markerLenght
 		marker.clear()
-		marker.add(message[0 + cpt])
-		marker.add(message[1 + cpt])
-		marker.add(message[2 + cpt])
-		marker.add(message[3 + cpt])
-		if(len(marker) == 4 ):
-			print(cpt + 4)
-			return cpt + 4
-		cpt += 1 
+		while(temp > 0):
+			marker.add(message[cpt + temp])
+			temp -= 1
+		cpt += 1
+		if(len(marker) == markerLenght ):
+			return cpt + markerLenght
+	
 
-
-firstMarker(content)
+x = firstMarker(content)
+print(x)
